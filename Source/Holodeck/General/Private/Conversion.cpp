e02e0b98f37f5fdaf1dc70d4285525d4324a2327
Joshua Greaves <joshua.greaves@gmail.com>
2019-04-29 15:55:28 -0700

    added conversion files

---
+++ Source/Holodeck/General/Private/Conversion.cpp
@@ -0,0 +1,51 @@
+#include "Holodeck.h"
+#include "Conversion.h"
+
+FVector ConvertLinearVector(FVector Vector, ConvertType Type) {
+
+	float ScaleFactor = 1.0;
+	if (Type == UEToClient)
+		ScaleFactor /= UEUnitsPerMeter;
+	else if(Type == ClientToUE)
+		ScaleFactor *= UEUnitsPerMeter;
+
+	Vector.X *= ScaleFactor;
+	Vector.Y *= -ScaleFactor; // Negative accounts for left handed coordinate system to right handed coordinate system change
+	Vector.Z *= ScaleFactor;
+	return Vector;
+}
+
+FVector ConvertAngularVector(FVector Vector, ConvertType Type) {
+
+	float ScaleFactor = 1.0;
+	if (Type == UEToClient)
+		ScaleFactor /= UEUnitsPerMeter;
+	else if (Type == ClientToUE)
+		ScaleFactor *= UEUnitsPerMeter;
+
+	Vector.X *= -ScaleFactor;
+	Vector.Y *= ScaleFactor;
+	Vector.Z *= -ScaleFactor;
+	return Vector;
+}
+
+
+FRotator ConvertAngularVector(FRotator Rotator, ConvertType Type) {
+	FVector Vec = Rotator.Vector();
+	FRotator Rot = ConvertAngularVector(Vec, Type).Rotation();
+	return Rot;
+}
+
+FVector ConvertTorque(FVector Vector, ConvertType Type) {
+
+	float ScaleFactor = 1.0;
+	if (Type == UEToClient)
+		ScaleFactor /= UEUnitsPerMeterSquared;
+	else if (Type == ClientToUE)
+		ScaleFactor *= UEUnitsPerMeterSquared;
+
+	Vector.X *= -ScaleFactor;
+	Vector.Y *= ScaleFactor;
+	Vector.Z *= -ScaleFactor;
+	return Vector;
+}
---
+++ Source/Holodeck/General/Public/Conversion.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include "Holodeck.h"
+
+const float UEUnitsPerMeter = 100.0;
+const float UEUnitsPerMeterSquared = 10000;
+
+enum ConvertType {UEToClient, ClientToUE, NoScale};
+
+FVector ConvertLinearVector(FVector Vector, ConvertType Type);
+
+
+FVector ConvertAngularVector(FVector Vector, ConvertType Type);
+
+FRotator ConvertAngularVector(FRotator Rotator, ConvertType);
+
+
+FVector ConvertTorque(FVector Vector, ConvertType Type);
